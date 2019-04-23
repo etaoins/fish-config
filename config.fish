d98ca2952c2cb51bef3d71c87b84d56a20b298af
Ryan Cumming <etaoins@gmail.com>
2019-04-23 13:24:34 +1000

    Initial commit

---
+++ config.fish
@@ -0,0 +1,7 @@
+set __fish_git_prompt_showcolorhints 1
+set __fish_git_prompt_showuntrackedfiles 1
+set __fish_git_prompt_showdirtystate 1
+set __fish_git_prompt_showstashstate 1
+set __fish_git_prompt_showupstream 1
+
+set fish_greeting
